224e6f305ff283a499712380bfc255511a911846
tribuonoasto <tri.nugroho@smma.co.id>
2023-05-11 16:44:09 +0700

    change store using comp api

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,18 +1,18 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 import api from "../services/http-client/api/index";
 
-export const useUserStore = defineStore("user", {
-  state: () => ({}),
-  getters: {},
-  actions: {
-    // User Login
-    async login(data) {
-      console.log(data);
-      // const res = await api.user.login(data)
-    },
-    async register(data) {
-      const res = await api.user.register(data);
-      console.log(res);
-    },
-  },
+export const useUserStore = defineStore("user", () => {
+  const user = ref({});
+  // User Login
+  async function login(data) {
+    console.log(data);
+    // const res = await api.user.login(data)
+  }
+  // Register
+  async function register(data) {
+    const res = await api.user.register(data);
+    console.log(res);
+  }
+  return { user, login, register };
 });
